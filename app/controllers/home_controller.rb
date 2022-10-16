49acc5a656835cca7caf85c1d21097616d1e696f
kelsy <kmp887@gmail.com>
2022-10-15 21:41:35 -0700

    Add basic search form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@
 
 class HomeController < ApplicationController
   def index
-    @location = LocationService.new(query: "512 N Killingsworth St, Portland, OR").call
+  end
+
+  def search
+    @location = LocationService.new(query: params[:query]).call
     @weather = WeatherService.new(latitude: @location[:latitude], longitude: @location[:longitude]).call
   end
 end
